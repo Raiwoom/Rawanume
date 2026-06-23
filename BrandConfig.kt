c90c05f693bf84a70dfc965017cbe76ec0de2678
Raiwoom <alexanderriqoo@gmail.com>
2026-06-23 04:07:56 +0300

    BrandConfig.kt

---
+++ BrandConfig.kt
@@ -0,0 +1,13 @@
+package com.rawamune.p2p
+
+import androidx.compose.ui.graphics.Color
+
+object BrandConfig {
+    const val APP_NAME = "Rawamune"
+    val ColorBrandTitle = Color(0xFF22C55E) // Сочный Cyber Green для логотипа и счетчиков
+    val ColorNeonStroke = Color(0xFF38BDF8) // Неоновый голубой контур шестеренки
+
+    const val TEXT_MAIN_BUTTON = "ОТКРЫТЬ СЕТЬ"
+    const val TEXT_TECH_TITLE = "Параметры ядра Rawamune"
+    const val TEXT_CLOSE = "Закрыть"
+}
